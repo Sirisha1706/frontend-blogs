bc4e0e8d87da28b5b099f6d88f8c4244f3130f60
Sirisha1706 <63629781+Sirisha1706@users.noreply.github.com>
2022-03-11 09:23:01 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Route ,BrowserRouter, Routes} from 'react-router-dom';
 
 import './index.css';
 import App from './App';
@@ -8,12 +7,6 @@ import Register from './components/Register/Register';
 import Create from './components/Create/Create';
 
 ReactDOM.render(
-        <BrowserRouter>
-        <Routes>
-        <Route exact path='/register' element={<Register/>}/>
-        <Route exact path='/create' element={<Create/>}/>
-        </Routes>
-        <App />
-        </BrowserRouter>, 
+        <App />,
         document.getElementById('root')
       );
