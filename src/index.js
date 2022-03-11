31211f4805dbac2da56cb5c7dbc4a8e7d1a3a490
Sirisha1706 <63629781+Sirisha1706@users.noreply.github.com>
2022-03-11 09:29:27 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
-import Register from './components/Register/Register';
-import Create from './components/Create/Create';
+
 
 ReactDOM.render(
         <App />,
